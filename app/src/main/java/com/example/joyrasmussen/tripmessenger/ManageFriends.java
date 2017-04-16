d7725b7ba4dcdb6f0d02c9fcc1148f514f848a25
Clay Holt <clayholt@gmx.com>
2017-04-16 16:11:21 -0400

    friends progress

--- app/src/main/java/com/example/joyrasmussen/tripmessenger/ManageFriends.java
+++ app/src/main/java/com/example/joyrasmussen/tripmessenger/ManageFriends.java
@@ -1,14 +1,23 @@
 package com.example.joyrasmussen.tripmessenger;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -37,10 +46,26 @@ public class ManageFriends extends AppCompatActivity {
     private DatabaseReference mDatabase, userReference;
     FirebaseUser user;
 
+    ListView listView;
+
+    TextView friendsTab, approveTab, pendingTab;
+
+    FirebaseListAdapter<User> adapter;
+
+    String currentView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_friends);
+
+        currentView = "friends";
+
+        friendsTab = (TextView) findViewById(R.id.friendsTab);
+        pendingTab = (TextView) findViewById(R.id.pendingTab);
+        approveTab = (TextView) findViewById(R.id.approveTab);
+
+        listView = (ListView) findViewById(R.id.friendsListView);
 
         friendName = (EditText) findViewById(R.id.addFriendET);
         mDatabase = FirebaseDatabase.getInstance().getReference();
@@ -56,18 +81,46 @@ public class ManageFriends extends AppCompatActivity {
                 if (dataSnapshot.hasChild(user.getUid())) {
                     userObject = dataSnapshot.child(userID).getValue(User.class);
 
-                } else {
-
                 }
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
             }
         });
 
+        View.OnClickListener tabClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView v = (TextView) view;
+                changeTabs(v.getText().toString());
+            }
+        };
+        friendsTab.setOnClickListener(tabClick);
+        approveTab.setOnClickListener(tabClick);
+        pendingTab.setOnClickListener(tabClick);
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("users");
+
+        adapter = new FirebaseListAdapter<User>(this, User.class, android.R.layout.simple_list_item_1, ref) {
+            @Override
+            protected void populateView(View view, User user, int position) {
+
+                ((TextView)view.findViewById(android.R.id.text1)).setText(user.getFullName());
+
+            }
+        };
+
+        listView.setAdapter(adapter);
+
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        adapter.cleanup();
+    }
+
 
     public void add(View v) {
 
@@ -103,5 +156,32 @@ public class ManageFriends extends AppCompatActivity {
 
         Intent i = new Intent(this, FindFriends.class);
         startActivity(i);
+    }
+
+    public void changeTabs(String choice){
+
+        switch (choice) {
+            case "Friends":
+                currentView = "Friends";
+                friendsTab.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.authui_colorAccent));
+                pendingTab.setTextColor(Color.BLACK);
+                approveTab.setTextColor(Color.BLACK);
+
+                break;
+            case "Pending":
+                currentView = "Pending";
+                pendingTab.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.authui_colorAccent));
+                friendsTab.setTextColor(Color.BLACK);
+                approveTab.setTextColor(Color.BLACK);
+
+                break;
+            case "Approve":
+                currentView = "Approve";
+                approveTab.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.authui_colorAccent));
+                friendsTab.setTextColor(Color.BLACK);
+                pendingTab.setTextColor(Color.BLACK);
+
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/joyrasmussen/tripmessenger/UserHolder.java
@@ -0,0 +1,16 @@
+package com.example.joyrasmussen.tripmessenger;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+
+public class UserHolder extends RecyclerView.ViewHolder {
+
+
+    //Dont think we need this class
+    public UserHolder(View view) {
+        super(view);
+
+
+    }
+}
